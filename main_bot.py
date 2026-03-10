2da7537cca9151c441031dd03ea2474e4ff6380c
Milan <132593561+MilanM-V@users.noreply.github.com>
2026-03-10 21:54:41 +0100

    Change ECART_MAX_VAGUE_MIN from 15 to 5 minutes

--- main_bot.py
+++ main_bot.py
@@ -75,7 +75,7 @@ LAST_STATS_UPDATE = None
 # LOGIQUE DE VAGUES PAR CLUSTERING (15 MIN)
 # ==========================================
 
-ECART_MAX_VAGUE_MIN = 15      # 2 matchs espacés de moins de 15 min = même vague
+ECART_MAX_VAGUE_MIN = 5      # 2 matchs espacés de moins de 15 min = même vague
 FORCE_ENVOI_MIN_AVANT = 5     # On force l'envoi 5 min avant le 1er match de la vague
 
 def parse_match_datetime(time_str):
