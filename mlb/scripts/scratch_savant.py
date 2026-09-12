09e932f8aec8749337f98bb50bd0323d5394f167
Milan <2507milan@gmail.com>
2026-09-12 15:14:42 +0200

    chore: delete useless and obsolete audit scripts

--- mlb/scripts/scratch_savant.py
+++
@@ -1,7 +0,0 @@
-import pybaseball
-print(dir(pybaseball))
-try:
-    df = pybaseball.statcast_batter_exitvelo_barrels(2024)
-    print(df.head())
-except Exception as e:
-    print(e)
--- mlb/scripts/scratch_savant2.py
+++
@@ -1,3 +0,0 @@
-import pybaseball
-df = pybaseball.statcast_batter_exitvelo_barrels(2024)
-print(df.columns.tolist())
